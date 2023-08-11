f29e094a126fa9bec99f0443b837390cf444e2ec
Samuel Mesquita da Fonseca <75632669+samuelMesquita@users.noreply.github.com>
2023-08-11 01:09:29 -0300

    Primeiro exercicio da lista1.

---
+++ src/AlgoritimoEstruturaDados.Lista_1/Model/Vendas.cs
@@ -0,0 +1,118 @@
+﻿using System.IO;
+
+namespace AlgoritimoEstruturaDados.Lista_1.Model
+{
+    public class Vendas
+    {
+
+        public Vendas()
+        {
+            VendasFeitas = new double[QuantidadeMesAno, QuantidadeDiasSemana]
+            {
+                { 1000.00, 1500.00, 1200.00, 1800.00 },
+                { 1600.00, 1300.00, 1700.00, 1400.00 },
+                { 1100.00, 1400.00, 1300.00, 1900.00 },
+                { 1800.00, 1200.00, 1500.00, 1700.00 },
+                { 1700.00, 1600.00, 1800.00, 1400.00 },
+                { 1300.00, 1400.00, 1200.00, 1700.00 },
+                { 1500.00, 1700.00, 1600.00, 1300.00 },
+                { 1900.00, 1200.00, 1400.00, 1500.00 },
+                { 1400.00, 1800.00, 1300.00, 1600.00 },
+                { 1700.00, 1500.00, 1400.00, 1200.00 },
+                { 1200.00, 1600.00, 1800.00, 1300.00 },
+                { 1500.00, 1400.00, 1700.00, 1600.00 }
+            };
+        }
+        public Vendas(double[,] VendasFeitas)
+        {
+            if (VendasFeitas.GetLength(0) != QuantidadeMesAno && VendasFeitas.GetLength(0) != QuantidadeDiasSemana)
+                throw new ArgumentException("A matriz repassada esta em formato invalido");
+
+            this.VendasFeitas = VendasFeitas;
+        }
+
+        public double[,] VendasFeitas { get; private set; }
+
+        private double[] TotalVendidoMes = new double[QuantidadeMesAno];
+
+        private double TotalVendidoAno = 0;
+
+        private double[] MelhoresVendasSemanaDoAno = new double[4];
+
+        private const int QuantidadeMesAno = 12;
+
+        private const int QuantidadeDiasSemana = 4;
+
+       
+
+        public double[] TotalDeVendasDoMes()
+        {
+            GetVendasDoMes();
+
+            return TotalVendidoMes;
+        }
+
+        public double TotalDeVendasDoAno()
+        {
+            GetVendasDoAno();
+
+            return TotalVendidoAno; 
+        }
+
+        public double SemanaMaisVendasAnos()
+        {
+            double semanaComMaisVendas = RecuperarMaiorVendaDaSemana();
+
+            return semanaComMaisVendas;
+        }
+
+        private void GetVendasDoMes()
+        {
+            ZerarValoresVendidos();
+            double vendasFeitasNoMes = 0;
+
+            for (int i = 0; i < QuantidadeMesAno; i++)
+            {
+                for (int j = 0; j < QuantidadeDiasSemana; j++)
+                {
+                    vendasFeitasNoMes += this.VendasFeitas[i, j];
+
+                    this.MelhoresVendasSemanaDoAno[j] += this.VendasFeitas[i, j];
+                }
+
+                this.TotalVendidoMes[i] = vendasFeitasNoMes;
+            }
+        }
+
+        private double RecuperarMaiorVendaDaSemana()
+        {
+            GetVendasDoMes();
+            double semanaComMaisVendas = 0;
+
+            foreach(var melhorVendaSemanaAno in MelhoresVendasSemanaDoAno)
+            {
+                if(melhorVendaSemanaAno > semanaComMaisVendas)
+                    semanaComMaisVendas = melhorVendaSemanaAno;
+            }
+
+            return semanaComMaisVendas;
+        }
+
+        private void GetVendasDoAno()
+        {
+            GetVendasDoMes();
+
+            double vendaAno = 0;
+            foreach (var vendaMes in TotalVendidoMes)
+                vendaAno += vendaMes;
+
+            this.TotalVendidoAno = vendaAno;
+        }
+
+        private void ZerarValoresVendidos()
+        {
+            TotalVendidoAno = 0;
+            TotalVendidoMes = new double[QuantidadeMesAno];
+        }
+    }
+}
---
+++ src/AlgoritimoEstruturaDados.Lista_1/Program.cs
@@ -0,0 +1,27 @@
+﻿//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+//1) Criar um programa que carregue uma matriz 12 x 4 com os valores (em reais) das vendas de uma loja de
+//carros, em que cada linha represente um mês do ano, e cada coluna, uma semana do mês. Para fins de
+//simplificação considere que cada mês possui somente 4 semanas. Calcule e imprima:
+//-Total vendido em cada mês do ano;
+//-Total vendido no ano;
+//-Considerando todas as vendas do ano, informe qual é a melhor semana para vender carros (1ª, 2ª, 3ª ou 4ª)
+
+using AlgoritimoEstruturaDados.Lista_1.Model;
+
+Vendas vendas = new Vendas();
+
+var totalVendidoMes = vendas.TotalDeVendasDoMes();
+
+var totalVendidoAno = vendas.TotalDeVendasDoAno();
+
+var semanaComMaisVendas = vendas.SemanaMaisVendasAnos();
+
+
+foreach (var totalMes in totalVendidoMes)
+Console.WriteLine("Total vendido no mês é: {0}", totalMes);
+
+Console.WriteLine("Semana com mais venda do ano: {0}", semanaComMaisVendas);
+
+Console.WriteLine("Total vendido no ano é: {0}", totalVendidoAno);
+Console.ReadKey();
